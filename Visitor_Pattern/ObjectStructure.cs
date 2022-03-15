49498e81c3ddfeb77a32a6cb3be730cc0e327a7e
jebarcha <jebarcha@hotmail.com>
2022-03-15 17:14:02 -0600

    Visitor Design Pattern - First Commit

---
+++ Visitor_Pattern/ObjectStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor_Pattern
+{
+    public class ObjectStructure
+    {
+        private List<IVisitableElement> _cart;
+        public ObjectStructure(List<IVisitableElement> items)
+        {
+            _cart = items;
+        }
+        public void RemoveItem(IVisitableElement item)
+        {
+            _cart.Remove(item);
+        }
+        public void ApplyVisitor(IVisitor visitor)
+        {
+            Console.WriteLine("\n------ Visitor Breakdown -------");
+            foreach (var item in _cart)
+            {
+                item.Accept(visitor);
+            }
+
+            visitor.Print();
+        }
+    }
+}
---
+++ Visitor_Pattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<IVisitableElement>
+            {
+                new Book(12345, 11.99),
+                new Book(78910, 22.79),
+                new Vinyl(11198, 17.99),
+                new Book(63254, 9.79)
+            };
+
+            var cart = new ObjectStructure(items);
+            var discountVisitor = new DiscountVisitor();
+            //foreach (var item in items)
+            //{
+            //    item.Accept(discountVisitor);
+            //}
+            //discountVisitor.Print();
+
+            var salesVisitor = new SalesVisitor();
+            //foreach (var item in items)
+            //{
+            //    item.Accept(salesVisitor);
+            //}
+            //salesVisitor.Print();
+
+            cart.ApplyVisitor(discountVisitor);
+            cart.ApplyVisitor(salesVisitor);
+
+            discountVisitor.Reset();
+            cart.RemoveItem(items[2]);
+            cart.ApplyVisitor(discountVisitor);
+        }
+    }
+}
